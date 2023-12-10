6c240b07ea207c804a6071d4f88c819b1c8614ad
Quốc Trần <tranaiquoc4499@gmail.com>
2023-12-10 13:50:03 +0700

    Invite member to team

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ Don't know where to start? First of all, read the /help 😊, and the tutorials 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.join({
+  res.json({
     message: "Hmm!",
   });
 });
